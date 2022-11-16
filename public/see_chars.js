e24c8979356ba08a2402003d1be3a02c35ed1a36
Haley Akin <wasatchtimpanogos@outlook.com>
2022-11-15 19:41:02 -0700

    740 save

--- public/see_chars.js
+++ public/see_chars.js
@@ -1,11 +1,34 @@
 const characterContainer = document.querySelector('#characters-container')
 const randomCharBtn = document.querySelector('#randomCharBtn')
 
+function buildCharacterElement (character) {
+    const firstName = document.createElement('div')
+    firstName.classList.add('first-name')
+    firstName.innerText = character.first_name
+
+    const lastName = document.createElement('div')
+    lastName.classList.add('last-name')
+    lastName.innerText = character.last_name
+
+    const race = document.createElement('div')
+    race.classList.add('race')
+    race.innerText = character.race
+
+    const element = document.createElement('div')
+    element.classList.add('character')
+    element.appendChild(firstName)
+    element.appendChild(lastName)
+    element.appendChild(race)
+    return element
+}
+
 function getCharacter(id) {
     axios.get(`http://localhost:5500/character/${id}`)
         .then(res => {
           const character = res.data[0]
-          characterContainer.innerText = JSON.stringify(character)   
+          const element = buildCharacterElement(character)
+          characterContainer.appendChild(element)
+          //characterContainer.innerText = JSON.stringify(character)   
         })
 }
 
@@ -13,7 +36,13 @@ function getCharacters() {
     axios.get(`http://localhost:5500/characters`)
         .then(res => {
           const characters = res.data
-          characterContainer.innerText = JSON.stringify(characters)   
+          characters.forEach(character => {
+            const element = buildCharacterElement(character)
+            characterContainer.appendChild(element)
+            //const divider = document.createElement('hr')
+            //characterContainer.appendChild(divider)
+          })
+          //characterContainer.innerText = JSON.stringify(characters)   
         })
 }
 
